703c956238072122d755ff7b55fc1be409377a04
David Taylor <dmtaylor2011@gmail.com>
2020-05-26 04:52:19 +0000

    use unsigned ints for ids and values

--- src/models.rs
+++ src/models.rs
@@ -2,7 +2,7 @@ use super::chrono;
 
 #[derive(Queryable)]
 pub struct Bet {
-    pub bet_id: i32,
+    pub bet_id: u32,
     pub bet_name: String,
     pub bet_description: Option<String>,
     pub bet_close: Option<chrono::NaiveDateTime>,
@@ -11,16 +11,16 @@ pub struct Bet {
 
 #[derive(Queryable)]
 pub struct Outcome {
-    pub outcome_id: i32,
+    pub outcome_id: u32,
     pub outcome_name: String,
     pub outcome_text: Option<String>,
-    pub bet_id: Option<i32>,
+    pub bet_id: Option<u32>,
     pub created_on: chrono::NaiveDateTime,
 }
 
 #[derive(Queryable)]
 pub struct User {
-    pub user_id: i32,
+    pub user_id: u32,
     pub username: String,
     pub user_description: Option<String>,
     pub is_banned: bool,
@@ -29,11 +29,11 @@ pub struct User {
 
 #[derive(Queryable)]
 pub struct Wager {
-    pub wager_id: i32,
-    pub bet_id: Option<i32>,
-    pub user_id: Option<i32>,
-    pub outcome_id: Option<i32>,
-    pub amount: i64,
+    pub wager_id: u32,
+    pub bet_id: Option<u32>,
+    pub user_id: Option<u32>,
+    pub outcome_id: Option<u32>,
+    pub amount: u64,
     pub created_on: chrono::NaiveDateTime,
 }
 
