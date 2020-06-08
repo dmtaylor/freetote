2ae70e750e5a1d3ed1e16eee70b8d5a0021cafe3
David Taylor <dmtaylor2011@gmail.com>
2020-06-08 06:36:48 +0000

    add boilerplate insertable structs

--- src/models.rs
+++ src/models.rs
@@ -1,5 +1,5 @@
 use super::chrono;
-use super::schema::bets;
+use super::schema::{bets, outcomes, users, wagers};
 
 #[derive(Queryable, AsChangeset, Debug)]
 #[primary_key(bet_id)]
@@ -19,31 +19,58 @@ pub struct NewBet<'a> {
     pub bet_close: Option<&'a chrono::NaiveDateTime>,
 }
 
-#[derive(Queryable)]
+#[derive(Queryable, AsChangeset, Debug)]
+#[primary_key(outcome_id)]
 pub struct Outcome {
-    pub outcome_id: u32,
+    pub outcome_id: i32,
     pub outcome_name: String,
     pub outcome_text: Option<String>,
-    pub bet_id: u32,
+    pub bet_id: i32,
     pub created_on: chrono::NaiveDateTime,
 }
 
-#[derive(Queryable)]
+#[derive(Insertable)]
+#[table_name="outcomes"]
+pub struct NewOutcome<'a> {
+    pub outcome_name: &'a str,
+    pub outcome_text: Option<&'a str>,
+    pub bet_id: &'a i32,
+}
+
+#[derive(Queryable, AsChangeset, Debug)]
+#[primary_key(user_id)]
 pub struct User {
-    pub user_id: u32,
+    pub user_id: i32,
     pub username: String,
     pub user_description: Option<String>,
     pub is_banned: bool,
     pub created_on: chrono::NaiveDateTime,
 }
 
-#[derive(Queryable)]
+#[derive(Insertable)]
+#[table_name="users"]
+pub struct NewUser<'a> {
+    pub username: &'a str,
+    pub user_description: Option<&'a str>,
+}
+
+#[derive(Queryable, AsChangeset, Debug)]
+#[primary_key(wager_id)]
 pub struct Wager {
-    pub wager_id: u32,
-    pub bet_id: u32,
-    pub user_id: u32,
-    pub outcome_id: u32,
-    pub amount: u64,
+    pub wager_id: i32,
+    pub bet_id: i32,
+    pub user_id: i32,
+    pub outcome_id: i32,
+    pub amount: i64,
     pub created_on: chrono::NaiveDateTime,
+}
+
+#[derive(Insertable)]
+#[table_name="wagers"]
+pub struct NewWager<'a> {
+    pub bet_id: &'a i32,
+    pub user_id: &'a i32,
+    pub outcome_id: &'a i32,
+    pub amount: &'a i64,
 }
 
